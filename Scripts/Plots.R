495031defb688cb8aa3cc2f89e0c1592a131a36b
StatsThoughts <bryant.yang98@gmail.com>
2018-10-05 16:39:54 +0100

    Removed NAs from plot

--- Scripts/Plots.R
+++ Scripts/Plots.R
@@ -3,5 +3,4 @@ load("FishLengths.RData")
 dat <- x
 
 dat$Age <- as.factor(dat$Age)
-
-ggplot(data = dat) + geom_boxplot(aes(Age, Length, color = Age))
+ggplot(data = dat[!is.na(dat$Age),]) + geom_boxplot(aes(Age, Length, color = Age))
