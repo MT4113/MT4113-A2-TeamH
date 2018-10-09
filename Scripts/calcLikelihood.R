7d419f09e2f054edfdd8a6368002d4e9ca244815
cjlcasey <43880291+cjlcasey@users.noreply.github.com>
2018-10-09 21:17:04 +0100

    Update - Initialise
    This is what I've done on the initialisation step for the practical. There's a couple of unnecessary things that I've included just for visualisation.
    "initials" gives initial estimates for the the unknown age values, with corresponding lengths in column 1. Data frame form for consistency. 
    "k.estimates" gives the result asked for in (2) and (3) for the task, in data frame form.
    I haven't catogorised the age values to make subsetting og data frame easier.
    
    Please feel free to fiddle!

--- Scripts/calcLikelihood.R
+++ Scripts/calcLikelihood.R
@@ -2,3 +2,82 @@ likelihood <- function(x){
 
 }
 
+#---------------------------------Initialising:---------------------------------
+
+# Set Up
+setwd("/Users/lemon7/Desktop/MT4113 Computing in Statistics/Assignments/Assignment 2/")
+load("FishLengths.RData")
+
+fishdata <- x
+# fishdata$Age <- factor(fishdata$Age)
+knownage <- na.omit(fishdata)                 # Where real ages have been given.
+fishdata[is.na(fishdata)] <- 0
+unknownage <- fishdata[which(fishdata$Age == 0),]
+
+# Plot for Visualisation of Overlap between Categories:
+plot.understanding <- function(){
+  # In function to keep Run clean.
+  par(mfrow = c(3,1))
+  hist(knownage[,2], xlab = "Fish Length (cm)", ylab = "Frequency", 
+       main = "Histogram of Known")
+  hist(fishdata[,2], xlab = "Fish Length (cm)", ylab = "Frequency", 
+       main = "Histogram of All")
+  
+  plot(knownage$Age, knownage$Length, xlab = "Age Category", 
+       ylab = "Fish Length")
+  
+  abline(h = max(knownage[knownage$Age ==1,2]), col = "blue")
+  abline(h = min(knownage[knownage$Age ==1,2]), col = "blue", , lty = 3)
+  abline(h = max(knownage[knownage$Age ==2,2]), col = "red")
+  abline(h = min(knownage[knownage$Age ==2,2]), col = "red", , lty = 3)
+  abline(h = max(knownage[knownage$Age ==3,2]), col = "green")
+  abline(h = min(knownage[knownage$Age ==3,2]), col = "green", lty = 3)
+  
+  par(mfrow = c(1,1))
+}
+
+# Function to assign unkownage[,2] with age category using dist of known:
+prob.category <- function(length, category = 1){
+  dist <- knownage[knownage$Age == category,]
+  mu <- mean(dist[,2]) 
+  sd <- var(dist[,2])^.5
+  prob <- dnorm(length, mu, sd)
+  return(prob)
+}
+
+initials.for.unknown <- function(x){
+  # Input intended to be unknownage[,2]
+  # Output to give dataframe of fish lengths against expected ages.
+  initials <- data.frame("Length" = x, "Age" = rep(0, 900))
+  probs <- matrix(rep(0, 2700),nrow = 900, ncol = 3)
+  probs[,1] <- prob.category(x,1)
+  probs[,2] <- prob.category(x,2)
+  probs[,3] <- prob.category(x,3)
+  
+  for (i in 1:900){
+    # Aim to vectorise
+    initials[i,2] <- which.max(probs[i,])
+  }
+  return(initials)
+}
+
+k.estimates <- function(){
+  # Input of the "initials" data frame from function "initials.for.unknown".
+  # Output will give table of sigmas, mus and lambdas for each class k for age.
+  
+  initials <- initials.for.unknown(unknownage[,2]) # Initialising vectors 
+  initials1 <- initials[which(initials[,2] == 1),]
+  initials2 <- initials[which(initials[,2] == 2),]
+  initials3 <- initials[which(initials[,2] == 3),]
+  N <- 900                                        #Excluding known ages.
+  
+  #Calculating mean and standard deviation values for each age class:
+  mu <- c(mean(initials1[,1]), mean(initials2[,1]), mean(initials3[,1]))
+  variance <- c(var(initials1[,1]), var(initials2[,1]), var(initials3[,1]))
+  sigma <- sqrt(variance)
+  lambda <- c(length(initials1[,1]), length(initials2[,1]), length(initials3[,1]))/N
+  
+  return(data.frame("mu" = mu, "sigma" = sigma, "lambda" = lambda))
+}
+# k.estimates() 
+
