d36c973212abee551dad8eaa18c7370dee440c36
StatsThoughts <bryant.yang98@gmail.com>
2018-10-12 12:55:49 +0100

    Added required init parameters to the teamER script

--- Scripts/teamEM.R
+++ Scripts/teamEM.R
@@ -1,4 +1,6 @@
-
-teamEM <- function(){
-  
+source("Scripts/calcLikelihood.R")
+teamEM <- function(data, epsilon = 1e-08, maxit = 1000){
+  # Data - dataframe of observations 
+  # epsilon - itteration stopping point
+  # maxit - max number of iterations
 }
