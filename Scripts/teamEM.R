5ee3a9f2962ab7c7b78abf946576622c9216fb8f
StatsThoughts <bryant.yang98@gmail.com>
2018-10-13 15:49:03 +0100

    Testing for 5 length but that doesnt work

--- Scripts/teamEM.R
+++ Scripts/teamEM.R
@@ -19,14 +19,13 @@ teamEM <- function(data, epsilon = 1e-08, maxit = 1000){
   # Initalize the Data ------------------------------------------------------
   # Need value for k_numb which is the number of factors in K
   k_numb <- 3 #length(unique(data[,2]))-1 #Numeric values + NA
-  k_table <- k.estimates(data,1)
-  k_table$mu <- c(1,100,1000)
-  k_table$sigma <- c(1,10,100)
+  ##k_table <- k.estimates(data,1)
+  k_table<- data.frame("mu" = c(1,2,3, 4,850),  "sigma" = c(1,1,1,1,10), "lambda" = c(.1,.01,.01, .20,.5))
+
+  #Need to intialize df, to be completed. This is a temp one ot test
+  df <- data.frame("length" = c(1:1080), "p1" = rep(0.1,1080), "p2" = rep(0.2,1080), "p3" = rep(0.3,1080), "p3" = rep(0.4,1080))
   
-  #Need to intialize df, to be completed. This is a temp one ot test 
-  df <- data.frame("length" = c(1:1080), "p1" = rep(0.1,1080), "p2" = rep(0.2,1080), "p3" = rep(0.3,1080))
-  
-  print(k_table) #this is for testin gpurposes
+  inits<-k_table #this is for testin gpurposes
   
   # Loop --------------------------------------------------------------------
 
@@ -52,7 +51,7 @@ teamEM <- function(data, epsilon = 1e-08, maxit = 1000){
   
   # Return conditions 
   output <- list(estimates = k_table, 
-                inits = NA, 
+                inits = inits, 
                 converged = converged,
                 posterior = NA, #df,
                 likelihood = l1)
