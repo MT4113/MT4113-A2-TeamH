86cb6fef6b46aa63c4ce0e4485b08707f85a120c
cjlcasey <43880291+cjlcasey@users.noreply.github.com>
2018-10-13 09:36:53 +0100

    Update Plots.R
    wrong function had been moved. added legend

--- Scripts/Plots.R
+++ Scripts/Plots.R
@@ -5,25 +5,33 @@ dat <- x
 dat$Age <- as.factor(dat$Age)
 ggplot(data = dat[!is.na(dat$Age),]) + geom_boxplot(aes(Age, Length, color = Age))
 
-# Plot for Visualisation of Overlap between Categories:
-plot.understanding <- function(){
-  # In function to keep Run clean.
-  par(mfrow = c(3,1))
-  hist(knownage[,2], xlab = "Fish Length (cm)", ylab = "Frequency", 
-       main = "Histogram of Known")
-  hist(fishdata[,2], xlab = "Fish Length (cm)", ylab = "Frequency", 
-       main = "Histogram of All")
+# Required Plots
+plot.required <- function(dat = x){
+  # The aim here is to return the plots that are required in Task 1: Data Expl.
   
-  plot(knownage$Age, knownage$Length, xlab = "Age Category", 
-       ylab = "Fish Length")
+  # Histogram
+  par(mfrow = c(2,1))
+  hist(dat[,2], xlab = "Fish Length (cm)", ylab = "Frequency", 
+       main = "Histogram of Fish Lengths")
   
-  abline(h = max(knownage[knownage$Age ==1,2]), col = "blue")
-  abline(h = min(knownage[knownage$Age ==1,2]), col = "blue", , lty = 3)
-  abline(h = max(knownage[knownage$Age ==2,2]), col = "red")
-  abline(h = min(knownage[knownage$Age ==2,2]), col = "red", , lty = 3)
-  abline(h = max(knownage[knownage$Age ==3,2]), col = "green")
-  abline(h = min(knownage[knownage$Age ==3,2]), col = "green", lty = 3)
+  # Scatter Plot
+  plot(dat[,3], dat[,2], xaxt = "n", xlab = "Fish Age Category", 
+       ylab = "Fish Length (cm)",
+       main = "Plot of Age against Length in Fish")
+  axis(1, at = c(1,2,3))
+  legend(1, 80, legend = c("Category 1", "Category 2", "Category 3"), 
+         col = c("blue", "red", "green"), lty = 1:1, cex = .7)
+
+  # Some guidelines:
+  abline(h = max(known(dat)[known(dat)[,3] ==1,2]), col = "blue")
+  abline(h = min(known(dat)[known(dat)[,3] ==1,2]), col = "blue", , lty = 3)
+  abline(h = max(known(dat)[known(dat)[,3] ==2,2]), col = "red")
+  abline(h = min(known(dat)[known(dat)[,3] ==2,2]), col = "red", , lty = 3)
+  abline(h = max(known(dat)[known(dat)[,3] ==3,2]), col = "green")
+  abline(h = min(known(dat)[known(dat)[,3] ==3,2]), col = "green", lty = 3)
   
   par(mfrow = c(1,1))
 }
-plot.understanding()
+
+
+plot.required()
