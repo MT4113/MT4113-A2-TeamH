72e0238609c7349c822b3dbf9e0f8c030ff7d749
StatsThoughts <bryant.yang98@gmail.com>
2018-10-14 01:34:59 +0100

    Rewrite and clean

---
+++ Project 2 Rewrite/Rewrite/functions.R
@@ -0,0 +1,132 @@
+load("FishLengths.RData") 
+
+init_data_ests <- function(data){
+  # Initalizes the data, returns table of mu, sigma and lambda estimates based on known values
+  # Data:
+  #   Col1 - the fish IDs
+  #   Col2 - The Fish lengths 
+  #   Col3 - The age class they are in (k = ...?). -1 if unknown  
+  
+  ages <- unique(data$Age)
+  ages <- ages[ages != -1]  #removes unknown ages
+  ages <- ages[order(ages)] #Ensures lowest age is first to highest
+  
+  mu <- rep(NA, length(ages))
+  sigma <- rep(NA, length(ages))
+  lambda <- rep(NA, length(ages))
+  
+  for (i in c(1:length(ages))){
+    mu[i] <- mean(data[(data$Age == ages[i]),2])
+    sigma[i] <- sd(data[(data$Age == ages[i]),2])
+    lambda[i] <- length(data[(data$Age == ages[i]),2])
+  }
+  
+  lambda <- lambda/length(data[(data[,3] != -1),2])
+  
+  return(data.frame("mu" = mu, "sigma" = sigma, "lambda" = lambda))
+  
+}
+
+init_prob_ests <- function(data, k_table, k_numb){
+  # Data:
+  #   Col1 - the fish IDs
+  #   Col2 - The Fish lengths 
+  #   Col3 - The age class they are in (k = ...?). -1 if unknown 
+  
+  unknown_dat <- data[data$Age == -1,] #Sifts for only unknown data
+  N <- length(unknown_dat$Age) #total number of unknown data
+  
+  p_mat <- matrix(rep(NA,k_numb*N), ncol = k_numb) #Matrix of probabilites
+  
+  #based on k_table, assign probabilites 
+  for (i in c(1:k_numb)){
+    p_mat[,i] <- dnorm(unknown_dat$Length, k_table[i,"mu"], k_table[i,"sigma"])
+  }
+  
+  #Find most suitable class for each value
+  unknown_dat$Age <- apply(p_mat, 1, function(x){which.max(x)})
+  
+  ages <- unique(unknown_dat$Age)
+  ages <- ages[order(ages)]
+  
+  
+  for (i in c(1:length(ages))){
+    k_table$lambda[i] <- length(unknown_dat$Age[unknown_dat$Age == ages[i]])/N
+    k_table$mu[i] <- mean(unknown_dat$Length[unknown_dat$Age == ages[i]])
+    k_table$sigma[i] <- sd(unknown_dat$Length[unknown_dat$Age == ages[i]])
+  }
+  
+  return(k_table)
+}
+
+prob_ests <- function(data, k_table, k_numb){
+  # Data: - ONLY UNKNOWN DATA IS INPUT
+  #   Col1 - the fish IDs
+  #   Col2 - The Fish lengths 
+  #   Col3 - The age class they are in (k = ...?). -1 if unknown 
+  
+  N <- length(data$Age)
+  p_mat <- matrix(rep(NA,k_numb*N), ncol = k_numb) #Matrix of probabilites
+  
+  for (i in c(1:k_numb)){
+    p_mat[,i] <- dnorm(data$Length, k_table[i,"mu"], k_table[i,"sigma"])
+  }
+  
+  denomSums <- apply(p_mat, 1, sum)
+  
+  for (i in c(1:k_numb)){
+    p_mat[,i] <- p_mat[,i] * k_table[i,"lambda"]/denomSums
+  }
+  output <- data.frame(data[,c(1:2)], p_mat)
+  return(output)
+}
+
+max_ests <- function(prob_table, k_table, k_numb){
+  # Data: - ONLY UNKNOWN DATA IS INPUT
+  #   Col1 - the fish IDs
+  #   Col2 - The Fish lengths 
+  #   Col3 to 3+k_numb - probs it is in each age class k
+  
+  N <- length(prob_table$Length)
+  
+  probs <- prob_table[,c(-1,-2)] #simply to only probabilites 
+  lengths <- prob_table[,"Length"] #Can add lengths and stuff to the bottom if we want to extend for true values
+  
+  
+  baseProbSum<- apply(probs, 2, sum) #sums of all probs based on age class
+  
+
+  #Mean Calculations
+  numerMeans <- apply(probs, 2, function(x,y){return(sum(x*y))}, y = lengths)
+  k_table$mu <- numerMeans/baseProbSum
+  
+
+  #Sd Calculations
+  for(i in c(k_numb:1)){
+    k_table$sigma[i] <- sqrt( sum( (probs[i] * ((lengths - k_table$mu[i])^2)) / baseProbSum))
+  }
+
+  #Lambida Calculations 
+  k_table$lambda <- baseProbSum/N
+  
+  return(k_table)
+}
+
+likelihood <- function(unknown_dat, k_table, k_numb){
+  # Data: - ONLY UNKNOWN DATA IS INPUT
+  #   Col1 - the fish IDs
+  #   Col2 - The Fish lengths 
+  #   Col3 - The age class they are in (k = ...?). -1 if unknown  
+  
+  init <- 0 
+  k_table_logged <- k_table
+  k_table_logged$lambda <- log(k_table_logged$lambda)
+  
+  init <- sapply(unknown_dat$Length, normalFunc, k_table = k_table_logged)
+  return(sum(init))
+}
+
+normalFunc <- function(x, k_table){
+  return(sum(k_table$lambda + dnorm(x, k_table$mu, k_table$sigma, log = TRUE)))
+}
+
---
+++ Project 2 Rewrite/Rewrite/teamEM.R
@@ -0,0 +1,64 @@
+source("functions.R")
+
+teamEM <- function(data, epsilon = 1e-08, maxit = 1000){
+  # Data:
+  #   Col1 - the fish IDs
+  #   Col2 - The Fish lengths 
+  #   Col3 - The age class they are in (k = ...?). NA if unknown. k > 0  
+  # epsilon:  convergence criterion
+  # maxit: max iterations
+
+# Error Checking ----------------------------------------------------------
+    #Need something to ensure that inputs are correct
+    #Also need to ensure ages are != -1 and are positive
+    #Also need to ensure columns are named aptly, as we call by NAME
+
+# Data initalization ------------------------------------------------------
+  
+  #Sets Unknown ages to age 0. 
+  data[is.na(data$Age),3] <- -1
+  data$Age <- as.factor(data$Age)
+  
+  k_numb <- length(unique(data$Age))-1 #Gives total numb of k values 
+
+  #input - Dataframe data
+  #Out - matrix containing the estimates of stdev, mu and lambida
+  k_mat <- init_data_ests(data)
+  
+  #input - dataframe data
+  #output - lamda values 
+  k_mat <- init_prob_ests(data,k_mat,k_numb) #FOR TESTING ITS COMMENTED OUT
+  
+  #input - the dataframe data 
+  #output - dataframe of col1 - ID col2 - lengths, col3 onwards, one column for each inital probabilty labeled X#
+  #data_probs
+  
+  #Seperate known and unknown data
+  unknown_dat <- data[data$Age == -1,]
+
+# Loop --------------------------------------------------------------------
+
+  l2 <- likelihood(unknown_dat, k_mat, k_numb)
+  l1 <- l2 + (2*epsilon) 
+  while((abs(l2-l1) > epsilon) & maxit >0){
+    # Reassign exit conditions 
+    maxit <- maxit - 1 
+    #l2 <- l1
+    
+    #input - the dataframe data 
+    #output - dataframe of col1 - ID col2 - lengths, col3 onwards, one column for each inital probabilty labeled X#
+    prob_table <- prob_ests(unknown_dat, k_mat, k_numb)
+    
+    #input - probabilty table and k_mat and K_numb
+    #output, a new k_mat updated
+    
+    #For some reason i dont fully turst htis function 
+    k_mat <- max_ests(prob_table, k_mat, k_numb)
+    
+    #Input - 
+    #Output - values for likelihood in l2, this is LOG likelihood
+    l1 <- likelihood(unknown_dat, k_mat, k_numb)
+  }  
+  
+  return(list(l1-l2, head(prob_table), k_mat))
+}
