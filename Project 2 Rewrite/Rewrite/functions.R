075d2e0fe97f7465b643997c9c1771518704adb5
StatsThoughts <bryant.yang98@gmail.com>
2018-10-15 11:37:12 +0100

    Fixed numerous bugs present in the code. function now converges

--- Project 2 Rewrite/Rewrite/functions.R
+++ Project 2 Rewrite/Rewrite/functions.R
@@ -65,18 +65,20 @@ prob_ests <- function(data, k_table, k_numb){
   #   Col2 - The Fish lengths 
   #   Col3 - The age class they are in (k = ...?). -1 if unknown 
   
-  N <- length(data$Age)
+  N <- length(data$Age) #Total number of unknown ages in the thing
   p_mat <- matrix(rep(NA,k_numb*N), ncol = k_numb) #Matrix of probabilites
   
-  for (i in c(1:k_numb)){
+  for (i in c(1:k_numb)){ 
     p_mat[,i] <- dnorm(data$Length, k_table[i,"mu"], k_table[i,"sigma"])
+    p_mat[,i] <- (p_mat[,i] * k_table[i,"lambda"])
   }
   
   denomSums <- apply(p_mat, 1, sum)
   
-  for (i in c(1:k_numb)){
-    p_mat[,i] <- p_mat[,i] * k_table[i,"lambda"]/denomSums
+  for (i in c(1:k_numb)){ #Can change this to an apply function 
+    p_mat[,i] <- p_mat[,i]/denomSums
   }
+  
   output <- data.frame(data[,c(1:2)], p_mat)
   return(output)
 }
@@ -95,17 +97,20 @@ max_ests <- function(prob_table, k_table, k_numb){
   
   baseProbSum<- apply(probs, 2, sum) #sums of all probs based on age class
   
-
+  
   #Mean Calculations
   numerMeans <- apply(probs, 2, function(x,y){return(sum(x*y))}, y = lengths)
   k_table$mu <- numerMeans/baseProbSum
   
-
+  
   #Sd Calculations
-  for(i in c(k_numb:1)){
-    k_table$sigma[i] <- sqrt( sum( (probs[i] * ((lengths - k_table$mu[i])^2)) / baseProbSum))
+  for(i in c(1:k_numb)){
+    k_table$sigma[i] <- sqrt( sum( (probs[,i] * ((lengths - k_table$mu[i])^2)) / baseProbSum[i]))
+    
+    #print(sqrt( sum( (probs[,i] * ((lengths - k_table$mu[i])^2)) / baseProbSum)))
+    #print(length((probs[,i] * ((lengths - k_table$mu[i])^2))))
   }
-
+  
   #Lambida Calculations 
   k_table$lambda <- baseProbSum/N
   
--- Project 2 Rewrite/Rewrite/teamEM.R
+++ Project 2 Rewrite/Rewrite/teamEM.R
@@ -7,23 +7,24 @@ teamEM <- function(data, epsilon = 1e-08, maxit = 1000){
   #   Col3 - The age class they are in (k = ...?). NA if unknown. k > 0  
   # epsilon:  convergence criterion
   # maxit: max iterations
-
-# Error Checking ----------------------------------------------------------
-    #Need something to ensure that inputs are correct
-    #Also need to ensure ages are != -1 and are positive
-    #Also need to ensure columns are named aptly, as we call by NAME
-
-# Data initalization ------------------------------------------------------
+  
+  # Error Checking ----------------------------------------------------------
+  #Need something to ensure that inputs are correct
+  #Also need to ensure ages are != -1 and are positive
+  #Also need to ensure columns are named aptly, as we call by NAME
+  
+  # Data initalization ------------------------------------------------------
   
   #Sets Unknown ages to age 0. 
   data[is.na(data$Age),3] <- -1
   data$Age <- as.factor(data$Age)
   
   k_numb <- length(unique(data$Age))-1 #Gives total numb of k values 
-
+  
   #input - Dataframe data
   #Out - matrix containing the estimates of stdev, mu and lambida
   k_mat <- init_data_ests(data)
+  k_mat_init <- k_mat
   
   #input - dataframe data
   #output - lamda values 
@@ -35,15 +36,15 @@ teamEM <- function(data, epsilon = 1e-08, maxit = 1000){
   
   #Seperate known and unknown data
   unknown_dat <- data[data$Age == -1,]
-
-# Loop --------------------------------------------------------------------
-
+  
+  # Loop --------------------------------------------------------------------
+  
   l2 <- likelihood(unknown_dat, k_mat, k_numb)
   l1 <- l2 + (2*epsilon) 
   while((abs(l2-l1) > epsilon) & maxit >0){
     # Reassign exit conditions 
     maxit <- maxit - 1 
-    #l2 <- l1
+    l2 <- l1
     
     #input - the dataframe data 
     #output - dataframe of col1 - ID col2 - lengths, col3 onwards, one column for each inital probabilty labeled X#
@@ -51,8 +52,7 @@ teamEM <- function(data, epsilon = 1e-08, maxit = 1000){
     
     #input - probabilty table and k_mat and K_numb
     #output, a new k_mat updated
-    
-    #For some reason i dont fully turst htis function 
+    #For some reason i now fully trust this function with my 3rd nonexistent child  
     k_mat <- max_ests(prob_table, k_mat, k_numb)
     
     #Input - 
@@ -60,5 +60,14 @@ teamEM <- function(data, epsilon = 1e-08, maxit = 1000){
     l1 <- likelihood(unknown_dat, k_mat, k_numb)
   }  
   
-  return(list(l1-l2, head(prob_table), k_mat))
+  
+  converged <- ifelse(abs(l2-l1) > epsilon, FALSE, TRUE)
+  
+  # Return conditions 
+  output <- list(estimates = k_mat, 
+                 inits = k_mat_init, 
+                 converged = converged,
+                 posterior = head(prob_table), #This table needs to be cleaned
+                 likelihood = l1)#This needs to be in a vector form 
+  return(output)
 }
