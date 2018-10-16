be59f73f6fa6bda456b75fda90c646842773d3dc
StatsThoughts <bryant.yang98@gmail.com>
2018-10-16 15:25:26 +0100

    Changed ll to be correct output format

--- Project 2 Rewrite/Rewrite/teamEM.R
+++ Project 2 Rewrite/Rewrite/teamEM.R
@@ -50,35 +50,37 @@ teamEM <- function(data, epsilon = 1e-08, maxit = 1000,
   
   # Loop --------------------------------------------------------------------
   
-  l1 <- likelihood(unknown_dat, k_mat, k_numb)
-  l2 <- l1 + (2*epsilon) 
-  while((abs(l2-l1) > epsilon) & maxit >0){
+
+  maxit_Total <- maxit 
+  ll_vec <- rep(NA, maxit_Total+1)
+  maxit <- 2
+  ll_vec[maxit] <- likelihood(unknown_dat, k_mat, k_numb)
+  ll_vec[maxit - 1] <- ll_vec[maxit] + (2*epsilon)
+
+  while((abs(ll_vec[maxit]-ll_vec[maxit-1]) > epsilon) & maxit <= maxit_Total){
     # Reassign exit conditions 
-    maxit <- maxit - 1 
-    l2 <- l1
+    maxit <- maxit + 1 
     
     #input - the dataframe data 
     #output - dataframe of col1 - ID col2 - lengths, col3 onwards, one column for each inital probabilty labeled X#
     prob_table <- prob_ests(unknown_dat, k_mat, k_numb)
-
     #input - probabilty table and k_mat and K_numb
     #output, a new k_mat updated
     #For some reason i now fully trust this function with my 3rd nonexistent child  
     k_mat <- max_ests(prob_table, k_mat, k_numb, inc_known_k_iter, known_dat)
-    
     #Input - 
     #Output - values for likelihood in l2, this is LOG likelihood
-    l1 <- likelihood(unknown_dat, k_mat, k_numb)
+    ll_vec[maxit] <- likelihood(unknown_dat, k_mat, k_numb)
   }  
   
   
-  converged <- ifelse(abs(l2-l1) > epsilon, FALSE, TRUE)
+  converged <- ifelse(abs(ll_vec[maxit]-ll_vec[maxit-1]) > epsilon, FALSE, TRUE)
   
   # Return conditions 
   output <- list(estimates = k_mat, 
                  inits = k_mat_init, 
                  converged = converged,
                  posterior = head(prob_table), #This table needs to be cleaned
-                 likelihood = l1)#This needs to be in a vector form 
+                 likelihood = ll_vec[c(2:maxit)]) 
   return(output)
 }
