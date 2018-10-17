5a39131533bb68250023235617766f24dfd7faf6
abhinav0397 <abhi7.nair@gmail.com>
2018-10-17 22:05:37 +0100

    Density edit

--- Project 2 Rewrite/Rewrite/Plots.R
+++ Project 2 Rewrite/Rewrite/Plots.R
@@ -7,9 +7,9 @@ Fish_Length <- x
 library(ggplot2)
 #BarPlot between Fish Length and Age
 Plot1 <- ggplot(data = Fish_Length, aes(x = Age, y = Length)) +
-         geom_bar(stat= "identity", fill = " steelblue ") +
-         xlab(" Age ") + ylab(" Fish Length in Cm ")+ 
-         theme_minimal() + ggtitle(" Barplot between Fish Length and Age")
+         geom_bar(stat= " identity ", fill = " steelblue ") +
+         xlab(" Age ") + ylab(" Fish Length in Cm ") + 
+         theme_minimal() + ggtitle(" Barplot between Fish Length and Age ")
 Plot1
 #Scatterplot between Fish Length and Age
 Plot2 <- ggplot(Fish_Length, aes(Age, Length))+
@@ -19,18 +19,22 @@ Plot2 <- ggplot(Fish_Length, aes(Age, Length))+
 Plot2
 
 #Histogram of Fish Length and it's count 
-Plot2 <- ggplot(Fish_Length, aes(x = Length)) +
+Plot3 <- ggplot(Fish_Length, aes(x = Length)) +
          geom_histogram(binwidth = 3) +
          ggtitle(" Frequency histogram of Fish Length ") +
          xlab(" Fish Length in Cm")+ ylab(" Count ")
-Plot2
-#Density Histogram of Fish Length          
-Plot3 <- ggplot(Fish_Length, aes(x = Length)) +
-         geom_histogram(binwidth = (5), colour = "black", fill = "white")
-         ggtitle(" Frequency histogram of Fish Length ") +
-         xlab(" Fish Length in Cm")+ ylab(" Count ")+ theme_dark()
-        
-Plot3        
-        
-      
+Plot3
+#Density Histogram of Fish Length
+
+X <- seq(14, 90, length.out=1000)
+Y <- with(Fish_Length, dnorm(X, mean(Length), sd(Length)))
+lines(X, Y, col = "red")
+Plot4 <- ggplot(Fish_Length, aes(x = Length, y = ..density.. )) +
+         geom_histogram(binwidth = (5), colour = " black ", fill = " steelblue ") +
+         geom_line(data = Fish_Length, aes(x = X, y = Y), color = "white") +
+         ggtitle(" Density histogram of Fish Length ") +
+         xlab(" Fish Length in Cm")+ ylab(" Density ")+ theme_dark() 
+
+Plot4        
+     
                  
