3213969ea8786342a08c865f0167ee5ce4cc15c2
abhinav0397 <abhi7.nair@gmail.com>
2018-10-17 16:47:14 +0100

    Update Project 2 Rewrite/Rewrite/Plots.R

---
+++ Project 2 Rewrite/Rewrite/Plots.R
@@ -0,0 +1,34 @@
+
+
+library(dplyr)
+#Renaming 'x' dataset to Fish_Length for convenience 
+Fish_Length <- x
+
+library(ggplot2)
+#BarPlot between Fish Length and Age
+Plot1 <- ggplot(data = Fish_Length, aes(x = Age, y = Length)) +
+         geom_bar(stat= "identity", fill = " steelblue ") +
+         xlab(" Age ") + ylab(" Fish Length in Cm ")+ 
+         theme_minimal() + ggtitle(" Scatterplot between Fish Length and Age")
+Plot1
+#Scatterplot between Fish Length and Age
+Plot2 <- ggplot(Fish_Length, aes(Age, Length))+
+         geom_point(aes(colour = factor(Age)))+
+         xlab(" Age" ) + ylab(" Fish Length in Cm ")+
+         theme_dark() + ggtitle(" Scatterplot between Fish Length and Age")
+Plot2
+
+#Histogram of Fish Length and it's count 
+Plot2 <- ggplot(Fish_Length, aes(x = Length)) +
+         geom_histogram(binwidth = 3) +
+         ggtitle(" Frequency histogram of Fish Length ") +
+         xlab(" Fish Length in Cm")+ ylab(" Count ")
+Plot2
+#Density Histogram of Fish Length          
+Plot <- ggplot(Fish_Length, aes(x = Length)) +
+        geom_histogram(binwidth = 3) +
+        ggtitle(" Frequency histogram of Fish Length ") +
+        xlab(" Fish Length in Cm")+ ylab(" Count ")
+        
+      
+                 
