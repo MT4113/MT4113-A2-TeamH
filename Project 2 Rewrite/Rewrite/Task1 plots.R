b61d92310bc635c1da6f8cd5744409dc284c823b
abhinav0397 <abhi7.nair@gmail.com>
2018-10-18 02:38:38 +0100

    removal of bar

--- Project 2 Rewrite/Rewrite/Task1 plots.R
+++ Project 2 Rewrite/Rewrite/Task1 plots.R
@@ -5,27 +5,21 @@ library(dplyr)
 Fish_Length <- x
 
 library(ggplot2)
-#BarPlot between Fish Length and Age
-Plot1 <- ggplot(data = Fish_Length, aes(x = Age, y = Length)) +
-         geom_bar(stat= " identity ", fill = " steelblue ") +
-         xlab(" Age ") + ylab(" Fish Length in Cm ") + 
-         theme_minimal() + ggtitle(" Barplot between Fish Length and Age ")
-Plot1
-
 #Scatterplot between Fish Length and Age
-Plot2 <- ggplot(Fish_Length, aes(Age, Length))+
+Plot1 <- ggplot(Fish_Length, aes(Age, Length))+
          geom_point(aes(colour = factor(Age)))+
          xlab(" Age" ) + ylab(" Fish Length in Cm ")+
-         theme_dark() + ggtitle(" Scatterplot between Fish Length and Age")
-Plot2
+         theme_dark() + ggtitle(" Scatterplot between Fish Length and Age")+
+         stat_summary(fun.y="mean", geom="point")
+Plot1
 
 #Histogram of Fish Length and it's count 
-Plot3 <- ggplot(Fish_Length, aes(x = Length)) +
+Plot2 <- ggplot(Fish_Length, aes(x = Length)) +
          geom_histogram(binwidth = 3) +
          ggtitle(" Frequency histogram of Fish Length ") +
          xlab(" Fish Length in Cm")+ ylab(" Count ")
 
-Plot3
+Plot2
 
 #Creating X,Y to get the Normal Curve of Fish Length
 X <- seq(14, 90, length.out=1000)
@@ -33,13 +27,13 @@ Y <- with(Fish_Length, dnorm(X, mean(Length), sd(Length)))
 lines(X, Y, col = "red")
 
 #Density Histogram of Fish Length
-Plot4 <- ggplot(Fish_Length, aes(x = Length, y = ..density.. )) +
+Plot3 <- ggplot(Fish_Length, aes(x = Length, y = ..density.. )) +
          geom_histogram(binwidth = (5), colour = " black ", fill = " steelblue ") +
          geom_line(data = Fish_Length, aes(x = X, y = Y), color = "white") +
          ggtitle(" Density histogram of Fish Length ") +
          xlab(" Fish Length in Cm")+ ylab(" Density ")+ theme_dark() 
 
-Plot4        
+Plot3        
      
 
       
