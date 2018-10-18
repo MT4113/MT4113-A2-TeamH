18e6666c00d8c7d7ae0d5aba104b69505eff3235
abhinav0397 <abhi7.nair@gmail.com>
2018-10-18 07:08:32 +0100

    Box edit

--- Project 2 Rewrite/Rewrite/Task1 plots.R
+++ Project 2 Rewrite/Rewrite/Task1 plots.R
@@ -8,19 +8,23 @@ library(ggplot2)
 #Scatterplot between Fish Length and Age
 Plot1 <- ggplot(Fish_Length, aes(Age, Length)) +
          geom_point(aes(colour = factor(Age))) +
-         xlab(" Age " ) + ylab(" Fish Length in Cm ") +
+         xlab(" Age Category " ) + ylab(" Fish Length in Cm ") +
          theme_dark() + ggtitle(" Scatterplot between Fish Length and Age ")+
          stat_summary(fun.y = " mean ",
                       colour = " white", geom = " line ",group = 1)
+  
         
 Plot1
 
 #Box-Plot between Fish Length and Age
-Plot2 <-  ggplot(Fish_Length, aes(x = Age, y = Length)) +
-          geom_boxplot(fill = "#4271AE", colour = "#1F3552") +
+
+Fish_Length$Age <- as.factor(Fish_Length$Age)
+         
+Plot2 <- ggplot(data = Fish_Length[!is.na(Fish_Length$Age),]) + 
+          geom_boxplot(aes(Age, Length, fill = "Age"))+
           scale_x_discrete(name = " Age ") +
-          scale_y_continuous(name = "Fish Length in Cm") +
-          geom_point(na.rm = TRUE)
+          scale_y_continuous(name = "Fish Length in Cm")+
+          ggtitle("Boxplot of Fish length and Age")
 
 Plot2
 
