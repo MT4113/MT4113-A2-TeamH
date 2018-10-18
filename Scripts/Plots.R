ad6254168a07a238fc0d4fbd3378a9e03428ed57
StatsThoughts <bryant.yang98@gmail.com>
2018-10-18 15:21:07 +0100

    Moving some folders around

--- Scripts/Plots.R
+++ Scripts/Plots.R
@@ -1,37 +1,60 @@
+
+
+library(dplyr)
+#Renaming 'x' dataset to Fish_Length for convenience 
+Fish_Length <- x
+
 library(ggplot2)
-load("FishLengths.RData")
-dat <- x
+#BarPlot between Fish Length and Age
+Plot1 <- ggplot(data = Fish_Length, aes(x = Age, y = Length)) +
+<<<<<<< HEAD
+         geom_bar(stat= " identity ", fill = " steelblue ") +
+         xlab(" Age ") + ylab(" Fish Length in Cm ") + 
+         theme_minimal() + ggtitle(" Barplot between Fish Length and Age ")
+=======
+         geom_bar(stat= "identity", fill = " steelblue ") +
+         xlab(" Age ") + ylab(" Fish Length in Cm ")+ 
+         theme_minimal() + ggtitle(" Barplot between Fish Length and Age")
+>>>>>>> e8c9a11bed5620e4a902506892213a026993ea48
+Plot1
+#Scatterplot between Fish Length and Age
+Plot2 <- ggplot(Fish_Length, aes(Age, Length))+
+         geom_point(aes(colour = factor(Age)))+
+         xlab(" Age" ) + ylab(" Fish Length in Cm ")+
+         theme_dark() + ggtitle(" Scatterplot between Fish Length and Age")
+Plot2
 
-dat$Age <- as.factor(dat$Age)
-ggplot(data = dat[!is.na(dat$Age),]) + geom_boxplot(aes(Age, Length, color = Age))
+#Histogram of Fish Length and it's count 
+Plot3 <- ggplot(Fish_Length, aes(x = Length)) +
+         geom_histogram(binwidth = 3) +
+         ggtitle(" Frequency histogram of Fish Length ") +
+         xlab(" Fish Length in Cm")+ ylab(" Count ")
+<<<<<<< HEAD
+Plot3
 
-# Required Plots
-plot.required <- function(dat = x){
-  # The aim here is to return the plots that are required in Task 1: Data Expl.
-  
-  # Histogram
-  par(mfrow = c(2,1))
-  hist(dat[,2], xlab = "Fish Length (cm)", ylab = "Frequency", 
-       main = "Histogram of Fish Lengths")
-  
-  # Scatter Plot
-  plot(dat[,3], dat[,2], xaxt = "n", xlab = "Fish Age Category", 
-       ylab = "Fish Length (cm)",
-       main = "Plot of Age against Length in Fish")
-  axis(1, at = c(1,2,3))
-  legend(1, 80, legend = c("Category 1", "Category 2", "Category 3"), 
-         col = c("blue", "red", "green"), lty = 1:1, cex = .7)
+#Creating X,Y to get the Normal Curve of Fish Length
+X <- seq(14, 90, length.out=1000)
+Y <- with(Fish_Length, dnorm(X, mean(Length), sd(Length)))
+lines(X, Y, col = "red")
+#Density Histogram of Fish Length
+Plot4 <- ggplot(Fish_Length, aes(x = Length, y = ..density.. )) +
+         geom_histogram(binwidth = (5), colour = " black ", fill = " steelblue ") +
+         geom_line(data = Fish_Length, aes(x = X, y = Y), color = "white") +
+         ggtitle(" Density histogram of Fish Length ") +
+         xlab(" Fish Length in Cm")+ ylab(" Density ")+ theme_dark() 
 
-  # Some guidelines:
-  abline(h = max(known(dat)[known(dat)[,3] ==1,2]), col = "blue")
-  abline(h = min(known(dat)[known(dat)[,3] ==1,2]), col = "blue", , lty = 3)
-  abline(h = max(known(dat)[known(dat)[,3] ==2,2]), col = "red")
-  abline(h = min(known(dat)[known(dat)[,3] ==2,2]), col = "red", , lty = 3)
-  abline(h = max(known(dat)[known(dat)[,3] ==3,2]), col = "green")
-  abline(h = min(known(dat)[known(dat)[,3] ==3,2]), col = "green", lty = 3)
-  
-  par(mfrow = c(1,1))
-}
-
-
-plot.required()
+Plot4        
+     
+=======
+Plot2
+#Density Histogram of Fish Length          
+Plot3 <- ggplot(Fish_Length, aes(x = Length)) +
+         geom_histogram(binwidth = (5), colour = "black", fill = "white")
+         ggtitle(" Frequency histogram of Fish Length ") +
+         xlab(" Fish Length in Cm")+ ylab(" Count ")+ theme_dark()
+        
+Plot3        
+        
+      
+>>>>>>> e8c9a11bed5620e4a902506892213a026993ea48
+                 
--- Scripts/teamEM.R
+++ Scripts/teamEM.R
@@ -1,60 +1,288 @@
-source("Scripts/calcLikelihood.R")
-
-### PLEASE DO NOT TEST THIS YET, UNDER CONSTRUCTION, WONT WORK
-teamEM <- function(data, epsilon = 1e-08, maxit = 1000){
-  # Data - dataframe of observations. Must be define as the following format: 
-  #       Col 1: lengths of fishes
-  #       Col 2: Catagorical variable clasfification (age class k)
-  #       Null is there is no classification
-  # epsilon - itteration stopping point
-  # maxit - max number of iterations
+teamEM <- function(data, epsilon = 1e-08, maxit = 1000, 
+                   inc_known_init = FALSE, inc_known_iter = FALSE,
+                   inc_known_as_unknown_init = FALSE, 
+                   inc_known_as_unknown_iter = T, 
+                   inc_known_in_ll = FALSE,
+                   roundAge = FALSE){
   
-  #Design note, will best want to specify which columns and rows are used for observations and for k 
-
-  # Input checks ------------------------------------------------------------
-  # Need to check data is a dataframe of sufficent lengths and conditions 
-  # Need checks for epsilon and maxit
-
   
-  # Initalize the Data ------------------------------------------------------
-  # Need value for k_numb which is the number of factors in K
-  k_numb <- 3 #length(unique(data[,2]))-1 #Numeric values + NA
-  k_table <- k.estimates(data,1)
-  k_table$mu <- c(1,100,1000)
-  k_table$sigma <- c(1,10,100)
-  
-  #Need to intialize df, to be completed. This is a temp one ot test 
-  df <- data.frame("length" = c(1:1080), "p1" = rep(0.1,1080), "p2" = rep(0.2,1080), "p3" = rep(0.3,1080))
-  
-  print(k_table) #this is for testin gpurposes
-  
-  # Loop --------------------------------------------------------------------
-
-  l2 = 0
-  l1 = 10
-  
-  #Denote l2 as previous likelihood, l1 as newest likelihood
-  while(abs(l2-l1) > epsilon & (maxit > 0) ){
+  ###########################################################################
+  # The purpose of this function is to implement the Expectation 
+  # Maximization Algorithm described in the paper Do, C.B. and S. Batzoglou. 
+  # 2008. What is the expectation maximization algorithm? Nature Biotechnology 
+  # 26:897-899.
+  # 
+  # Arguments:
+  # 
+  #   data - Dataframe consisting of at least two columns named Age and Lengths.
+  #          Column Length must contain numeric data with no null values in the 
+  #          dataframe. Column Age must contain either positive numeric values 
+  #          for known ages or NA for unknown ages.
+  #   epsilon - A tolerance value to determine convergence of the expectation 
+  #             maximization algorithm. Value must be a positive numeric value. 
+  #             Default is 1e-08.
+  #   inc_known_init - Boolean value when set to TRUE includes known fish 
+  #                      ages in the initialization step of the algorithm when 
+  #                      estimating parameters mu, sigma and lambda. Default is 
+  #                      FALSE
+  #   inc_known_iter - Boolean value when set to TRUE includes known fish ages
+  #                      in the iteration and maximization step algorithm when 
+  #                      estimating parameters mu, sigma and lambda. 
+  #                      Default is TRUE
+  #   inc_known_as_unknown_init - Boolean value when set to TRUE includes known 
+  #                               fish ages in the initialization step of the 
+  #                               algorithm when estimating parameters mu, sigma
+  #                               and lambda. Values are treated as unknown 
+  #                               values, and have probabilities estimated by 
+  #                               the algorithm. Default is FALSE
+  #   inc_known_as_unknown_iter - Boolean value when set to TRUE includes known 
+  #                               fish ages in the iteration and maximization 
+  #                               step of the algorithm when estimating  
+  #                               parameters mu, sigma and lambda. Values are  
+  #                               treated as unknown values, and have  
+  #                               probabilities estimated by the algorithm.
+  #                               Default is FALSE
+  #   inc_known_in_ll - Boolean value when set to TRUE includes the values of 
+  #                     known fish ages in the calculation of the log likelihood 
+  #                     based on parameters estimated by the maximization 
+  #                     function. Default is FALSE
+  #   roundAge - Boolean value when set to TRUE rounds the value of Ages to the 
+  #              nearest whole number during initialization. Can be used if  
+  #              exact ages are specified in data, but discrete age categories 
+  #              are to be used. Default is FALSE
+  # 
+  # Outputs: 
+  #   estimates - Estimates of mu, sigma, and lambda from the expectation 
+  #               maximization algorithm output in a dataframe where each age 
+  #               category is represented by a row, and mu, sigma and lambda 
+  #               estimates are represented by the columns.
+  #   inits - initial estimates of mu, sigma, and lambda output in a dataframe
+  #           where each age category is represented by a row, and mu, sigma 
+  #           and lambda estimates are represented by the columns.
+  #   converged - Boolean value indicating if the algorithm converged given 
+  #               epsilon and maxit values
+  #   posterior - List of posterior probabilities calculated based on 
+  #               implementation used during the iteration step (the value of 
+  #               inc_known_as_unknown_iter). Probabilities are listed first by 
+  #               known and then by unknown values of the Age column from data 
+  #               in the order the values appear in the dataframe. For values 
+  #               with given age classes, they are assigned the posterior 
+  #               probability value of 1 in the age class they are from and 0 
+  #               otherwise.
+  #   likelihood - Vector of log likelihood results for each iteration of the 
+  #                expectation maximization algorithm. The first element is the 
+  #                first log likelihood result and the last element is the log 
+  #                likelihood at convergence.
+  # 
+  # Implementation Notes:
+  #   * The output for posterior is ordered first by all known Age classes then 
+  #     by all unknown age classes. Within these groupings the output is sorted 
+  #     by the order of occurance. So the output would be ordered as 
+  #     (Known Obvs 1, Known Obvs 2 ... Known Obvs n, Unknown Obvs 1, 
+  #       Unknown Obvs 1 ... Unknown Obvs m). This ordering is for 
+  #     inc_known_as_unknown_iter = FALSE as known data is treated as 
+  #     unknown data when inc_known_as_unknown_iter = TRUE
+  #   * The columns for estimates and inits are numbed based on the value for  
+  #     the age category. 
+  #   * If a sigma estimate cannot be obtained based on the known data, it is 
+  #     set to 10 and a warning is produced.
+  #   * If a sigma estimate cannot be obtained based on the first guesses to the 
+  #     age category based on first estimates from known values, then the value 
+  #     for sigma taken from the first initial estimate is used instead and a 
+  #     warning is produced. 
     
-    # Reassign exit conditions 
-    maxit <- maxit - 1 
-    l2 <- l1
-    
-    #Probailty calculations 
-    df <- prob_expectations(df, k_table, k_numb)
-    k_table <- max_Ests(df)
-
-    # Likelihood calculation
-    l1 <- Likelihood_Eval(data, k_table)
+  
+  # Source all the functions located in functions.R
+  source("functions.R", local = TRUE)
+ 
+  # Error Checking ----------------------------------------------------------
+  # Source all the functions located in ErrorChecks.R
+  source("ErrorChecks.R", local = TRUE)
+  
+  # Dataframe Check (ensures inputs are valid)
+  if(!df_check(data)){stop("Invalid dataset for data")}
+  
+  # Numeric Check (ensures inputs are valid)
+  if (!(numeric_Check(epsilon, F, T))){stop("Invalid arguments for epsilon")}
+  if (!(numeric_Check(maxit, T, T))){stop("Invalid arguments for maxit")}
+  
+  # Boolean Check (ensures inputs are valid)
+  if(!(boolean_check(inc_known_iter))){
+    stop("Invalid arguments for inc_known_iter")
+    }
+  if(!(boolean_check(inc_known_init))){
+    stop("Invalid arguments for inc_known_init")
+    }
+  if(!(boolean_check(inc_known_as_unknown_iter))){
+    stop("Invalid arguments for inc_known_as_unknown_iter")
+    }
+  if(!(boolean_check(inc_known_as_unknown_init))){
+    stop("Invalid arguments for inc_known_as_unknown_init")}
+  
+  # Checks to ensure there are no conflicts including known values as both 
+  # known and unknown values in calculations
+  if(inc_known_as_unknown_init & inc_known_init){
+    stop(paste("inc_known_as_unknown_init and inc_known_init are both TRUE",
+               "Only one can be TRUE"))
+  }
+  if(inc_known_as_unknown_iter & inc_known_iter){
+    stop(paste("inc_known_as_unknown_iter and inc_known_iter are both TRUE",
+               "Only one can be TRUE"))
+  }
+  if(inc_known_in_ll & inc_known_as_unknown_iter){
+    stop(paste("inc_known_in_ll and inc_known_as_unknown_iter are both TRUE",
+               "only one can be TRUE"))
   }
   
-  converged <- ifelse(abs(l2-l1) > epsilon, FALSE, TRUE)
+  # Data initalization ------------------------------------------------------
   
-  # Return conditions 
-  output <- list(estimates = k_table, 
-                inits = NA, 
-                converged = converged,
-                posterior = NA, #df,
-                likelihood = l1)
+  # Sets Unknown ages from data to equal -1. 
+  data$Age[is.na(data$Age)] <- -1
+  
+  #Round ages if ages based on roundAge
+  if(roundAge){data$age <- round(data$Age)} 
+  
+  # Preprocessing, grouping ages as a factor
+  data$Age <- as.factor(data$Age) 
+  
+  #Removes any uneeded columns to reduce possible RAM burden 
+  data <- data.frame(Length = data$Length, Age = data$Age) 
+
+  #Seperate known and unknown data into two variables
+  unknown_dat <- data[data$Age == -1,] 
+  known_dat <- data[data$Age != -1,]
+  
+  # Gets all the unique age catagories found in the dataset 
+  uniq_ages <- unique(data$Age)
+  uniq_ages <-uniq_ages[order(uniq_ages)]
+  uniq_ages <- uniq_ages[uniq_ages != -1]
+  
+  # Determine the number of age catagories in the dataset
+  k_numb <- length(uniq_ages) 
+  
+
+  # Obtain intial estimates for mu, sigma and lambda from known data
+  k_mat <- init_data_ests(data, uniq_ages)
+  
+  # Check case for if sigma cannot be determined due to insufficent 
+  # inital parameters (ex, only one known observation for Age = 1)
+  if(length(k_mat$sigma[is.na(k_mat$sigma)]) > 0){ #If sigma cant be determined
+    k_mat$sigma[is.na(k_mat$sigma)] <- 10
+    warning(paste("Insuffient inital observations to generate an estimate for" ,
+                  "all values of sigma.\n  Undertermined values of Sigma are", 
+                  "set to 10 during initalization."))
+  }
+
+  # Check case for if known data needs to be included due to the value of 
+  # inc_known_as_unknown_init
+  unknown_dat_case <- unknown_dat
+  if(inc_known_as_unknown_init){unknown_dat_case <- rbind(known_dat, 
+                                                          unknown_dat_case)}
+  
+  # Recalculates estimates for mu, sigma and lambda based on inital guesses of
+  # the age of unknown data. Assigned to a new variable in case we need to 
+  # revert back to previous estimates
+  k_mat2 <- init_prob_ests(k_mat,k_numb, inc_known_init, 
+                          unknown_dat_case ,known_dat, uniq_ages) 
+  k_mat_init <- k_mat2
+
+  # Check case if sigma cannot be determined due to insufficent estimates 
+  if(length(k_mat2$sigma[is.na(k_mat2$sigma)]) > 0){
+    
+    # NA values are replaced with values determined from previous estimates
+    k_mat2$sigma[is.na(k_mat2$sigma)] <- k_mat$sigma[is.na(k_mat2$sigma)]
+    
+    #Generated warning
+    warning(paste("Insuffient estimated observations to generate an estimate ",  
+              "for sigma.\n  Undertermined values of Sigma are set to ", 
+              "initally calculated estimates shown in $init"))
+  }
+
+  
+  # Loop --------------------------------------------------------------------
+  
+  # Initalizes the vector to store the values of calculated log likelihoods
+  # and intializes values for the iterative part of the algorithim
+  maxit_Total <- maxit + 1 #Sets endpoint when to terminate loop
+  ll_vec <- rep(NA, maxit_Total+1) #Generates a log likelihood vector
+  maxit <- 2 #sets intial starting point
+  
+  # Initalizes the 2nd value of the log likelihood vector
+  ll_vec[maxit] <- likelihood(unknown_dat, k_mat, k_numb)
+  
+  # Case when inc_known_in_ll is TRUE
+  if(inc_known_in_ll){
+    ll_vec[maxit] <- ll_vec[maxit] + likelihood(known_dat, k_mat, k_numb)
+  }
+  
+  # Initalizes first value of the log likelihood vector to ensure that  
+  # the iteration beings 
+  ll_vec[maxit - 1] <- ll_vec[maxit] + (2*epsilon)
+  
+  # Check case in case there is a numerical overflow due to inital estimates
+  # generating likelihoods that are too unlikely, thus causing a -Inf
+  # to be generated for the sum. Terminates the program if it occurs
+  if(is.infinite(ll_vec[maxit])){
+    stop("Numerical overflow - Likelihood estimates cannot be determined")}
+  
+  # Check case for when inc_known_as_unknown_iter and values needed to be added
+  # for the iteration step
+  if(inc_known_as_unknown_iter){unknown_dat <- rbind(known_dat, unknown_dat)}
+
+  
+  # Iteration of the expectation maximization algorithim 
+  while((abs(ll_vec[maxit]-ll_vec[maxit-1]) > epsilon) & maxit <= maxit_Total){
+    
+    # Reassign exit conditions 
+    maxit <- maxit + 1 
+
+    # Calculate posterior probabilites
+    prob_table <- prob_ests(unknown_dat, k_mat, k_numb)
+    
+    # Calculate new parameter estimates
+    k_mat <- max_ests(prob_table, k_mat, k_numb, inc_known_iter, known_dat)
+
+    # Calculate new likelihood 
+    ll_vec[maxit] <- likelihood(unknown_dat, k_mat, k_numb)
+    if(inc_known_in_ll){ll_vec[maxit] <- likelihood(known_dat, k_mat, k_numb)}
+
+  }  
+  
+  #Test for convergence 
+  converged <- ifelse(abs(ll_vec[maxit]-ll_vec[maxit-1]) > epsilon, FALSE, TRUE)
+  
+  # Formatting the output of the posterior
+  prob_out <- prob_table[,c(-1,-2), drop = F]
+  
+  #Corner case that throws an error for some reason only for a single value
+  if (k_numb == 1){ 
+    prob_out <- data.frame("X1" = rep(1, length(data$Length)))
+  } else {
+    prob_out <- prob_table[,c(-1,-2), drop = F]
+  }
+  
+  # Case where posterior knowns are not used in the iteration step,
+  if(inc_known_as_unknown_iter == FALSE & (k_numb != 1)){
+    
+    #Gets the posteriors for all KNOWN values (permitation of 1,0,0,...,0)
+    known_post_prob <- sapply(uniq_ages, add_binary_cols, dat = known_dat$Age)
+    known_lengths <- data.frame(known_post_prob)
+    
+    # Assigns to the top of the posterior probabilites
+    prob_out <- rbind(known_lengths, prob_out)
+  } 
+  
+  # Formatting col/row names
+  colnames(prob_out) <- uniq_ages
+  rownames(prob_out) <- c() 
+  rownames(k_mat) <- uniq_ages
+  rownames(k_mat_init) <- uniq_ages
+  
+  # Return values 
+  output <- list(estimates = k_mat, 
+                 inits = k_mat_init, 
+                 converged = converged,
+                 posterior = (prob_out), 
+                 likelihood = ll_vec[c(3:maxit)]) 
   return(output)
 }
